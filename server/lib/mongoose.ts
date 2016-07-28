cd7a9ddca9e8ecf731ae8d86d5caddea6049d371
Alex Golubnychyi <oleksii.golubnychyi@gmail.com>
2016-07-28 02:09:19 -0400

    look at external mongo

--- server/lib/mongoose.ts
+++ server/lib/mongoose.ts
@@ -3,7 +3,7 @@ import * as mongoose from "mongoose";
 import * as Promise from "bluebird";
 let herokuMongoConnection = process.env.MONGOHQ_URL;
 
-mongoose.connect(herokuMongoConnection || /*"mongodb://hsuser2:558222546@ds031915.mlab.com:31915/hsdb" || */"mongodb://localhost/hearthstonedb", {
+mongoose.connect(herokuMongoConnection || "mongodb://hsuser2:558222546@ds031915.mlab.com:31915/hsdb" || "mongodb://localhost/hearthstonedb", {
     "server": {
         socketOptions: {
             keepAlive: 1
