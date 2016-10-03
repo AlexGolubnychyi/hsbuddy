fb7c43e595cf15a8679e6c9bc32f0c8e7b55a785
Alex G <ogolubnychyi@medavante.com>
2016-10-03 09:55:47 -0400

    fix ng2-bootstap so build passes

--- client/components/ng2-bootstrap/collapse/collapse.directive.ts
+++ client/components/ng2-bootstrap/collapse/collapse.directive.ts
@@ -34,30 +34,30 @@ export class CollapseDirective implements OnInit {
     // @HostBinding("style.height")
     // private height:string;
     @HostBinding("style.display")
-    private display: string;
+    display: string;
     // shown
     @HostBinding("class.in")
     @HostBinding("attr.aria-expanded")
-    private isExpanded: boolean = true;
+    isExpanded: boolean = true;
     // hidden
     @HostBinding("attr.aria-hidden")
-    private isCollapsed: boolean = false;
+    isCollapsed: boolean = false;
     // stale state
     @HostBinding("class.collapse")
-    private isCollapse: boolean = true;
+    isCollapse: boolean = true;
     // animation state
     @HostBinding("class.collapsing")
-    private isCollapsing: boolean = false;
+    isCollapsing: boolean = false;
 
     // @Input() private transitionDuration:number = 500; // Duration in ms
 
     @Input()
-    private set collapse(value: boolean) {
+    set collapse(value: boolean) {
         this.isExpanded = value;
         this.toggle();
     }
 
-    private get collapse(): boolean {
+    get collapse(): boolean {
         return this.isExpanded;
     }
 
