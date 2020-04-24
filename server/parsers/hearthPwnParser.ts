97c4b4ea940590e4980587f0785d405a477f834b
Alex G <ogolubnychyi@medavante.com>
2020-04-23 18:31:23 -0700

    Add ability to parse hearthpwn deck list

--- server/parsers/hearthPwnParser.ts
+++ server/parsers/hearthPwnParser.ts
@@ -2,15 +2,39 @@ import { getContent } from '../lib/request';
 import { BaseDeckParser, DeckData } from './base/baseDeckParser';
 
 class HearthPwnParser extends BaseDeckParser {
+    private baseUrl = 'https://hearthpwn.com';
     private deckRegex = /hearthpwn\.com\/decks\/([0-9]+)[a-z\-]*/;
+    private relativeDeckRegex = /\/decks\/[0-9a-z\-]+\/?$/;
+    private siteRegex = /hearthpwn\.com/;
     name = 'HearthPwn';
 
     canParse(url: string) {
-        return this.deckRegex.test(url);
+        return this.siteRegex.test(url);
     }
 
     protected getDeckData(url: string) {
-        return this.parseDeck(url).then(item => [item]);
+        if (this.deckRegex.test(url)) {
+            return this.parseDeck(url).then(item => [item]);
+        }
+        return this.parseDeckList(url);
+    }
+
+    private parseDeckList(url: string) {
+        console.log(`parsing ${url} for decks`);
+        return getContent(url)
+            .then($ => {
+                const unique = {};
+                $(`a`).each((inx: number, el: CheerioElement) => unique[($(el) as any).prop('href')] = true);
+                return Object.keys(unique).filter(linkUrl => this.relativeDeckRegex.test(linkUrl)).map(linkUrl => this.toAbsoluteUrl(linkUrl));
+            })
+            .map((deckUrl: string) => this.parseDeck(`${deckUrl}`), { concurrency: 2 });
+    }
+
+    private toAbsoluteUrl(relativeUrl: string) {
+        if (relativeUrl.startsWith('/')) {
+            relativeUrl = relativeUrl.slice(1);
+        }
+        return `${this.baseUrl}/${relativeUrl}`;
     }
 
     private parseDeck(url: string) {
